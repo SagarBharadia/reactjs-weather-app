9d13bc98a72ed14ac11e4bfa0a871d8059ef08f6
Sagar Bharadia <sagarbharadia@hotmail.com>
2020-06-04 14:35:36 +0100

    Styled App and Index pages ready for components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React from "react";
+
+const styles = {
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100vw",
+    height: "100vh",
+  },
+};
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div style={styles.wrapper}></div>;
 }
 
 export default App;
